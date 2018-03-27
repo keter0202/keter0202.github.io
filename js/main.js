f8d57a5f9d856db64c0a83df3f189c356b817279
keter <752761959@qq.com>
2018-03-27 17:47:59 +0800

    修改了导航条V1.0

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	"use strict";
+
+	$(document).ready(function(){
+		$('#fullpage').fullpage({
+			anchors:['firstPage', 'secondPage'],
+			menu: '#nav'
+		});
+	})
+});
