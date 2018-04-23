12891eb3b962d287c265da3976547e103c8749f2
keter <752761959@qq.com>
2018-04-23 22:39:15 +0800

    vue-cli
    vue-cli环境搭建

---
+++ src/components/header/header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <nav>
+      <ul>
+        <li>首页</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vheader'
+}
+</script>
+
+<style lang="scss" scoped>
+.header{
+  width: 100%;
+  height: auto;
+  nav{
+    ul{
+      li{
+        color: red;
+      }
+    }
+  }
+}
+</style>
