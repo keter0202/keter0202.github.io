b1f0053b717454b21d23b4b745b1bde09b9ca9f6
keter0202 <752761959@qq.com>
2018-11-03 14:58:05 +0800

    fl index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home/index' // 首页
+import fanLing from '@/views/fan_ling/index' // fan ling
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/fan-ling',
+      name: 'fanLing',
+      component: fanLing
     }
   ]
 })
---
+++ src/views/fan_ling/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fan-ling">
+    fanling
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fanLing'
+}
+</script>
+
+<style lang="scss" scoped>
+.fan-ling{
+  width: 100px;
+  height: auto;
+}
+</style>
